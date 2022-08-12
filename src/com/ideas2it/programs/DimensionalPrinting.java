328a6d2172c4b6b84a012c1b425f72d874512059
Aravinthan K <aravinthan.kandasamy@ideas2it.com>
2022-08-12 14:14:09 +0530

    [KA] code cleanup

--- src/com/ideas2it/programs/DimensionalPrinting.java
+++ src/com/ideas2it/programs/DimensionalPrinting.java
@@ -19,19 +19,16 @@ public class DimensionalPrinting {
             int columnNextSpace = 1;
             for (int j = 1; j <= columnInput; j++) {
                 if (i % rowNextSpace == 0 || j % columnNextSpace == 0) {
-                    System.out.print("*\t");
+                    System.out.print("*");
                     columnNextSpace += emptySpace + 1;
-                } else {
-                    System.out.print("\t");
                 }
+                System.out.print("\t");
             }
 
             if (i % rowNextSpace == 0) {
                 rowNextSpace += emptySpace + 1;
-                System.out.println();
-            } else {
-                System.out.println("\t");
             }
+            System.out.println();
         }
     }
 }
